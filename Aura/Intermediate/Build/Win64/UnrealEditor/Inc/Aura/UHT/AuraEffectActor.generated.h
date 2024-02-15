8745c82decc5856e947a8096d66667a1fd889265
Gavish Bansal <gavybansal9911@gmail.com>
2024-02-15 15:11:53 +0530

    Attributes

---
+++ Aura/Intermediate/Build/Win64/UnrealEditor/Inc/Aura/UHT/AuraEffectActor.generated.h
@@ -0,0 +1,71 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Actors/AuraEffectActor.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class AActor;
+class UPrimitiveComponent;
+struct FHitResult;
+#ifdef AURA_AuraEffectActor_generated_h
+#error "AuraEffectActor.generated.h already included, missing '#pragma once' in AuraEffectActor.h"
+#endif
+#define AURA_AuraEffectActor_generated_h
+
+#define FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_SPARSE_DATA
+#define FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execOnOverlap);
+
+
+#define FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_ACCESSORS
+#define FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAAuraEffectActor(); \
+	friend struct Z_Construct_UClass_AAuraEffectActor_Statics; \
+public: \
+	DECLARE_CLASS(AAuraEffectActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Aura"), NO_API) \
+	DECLARE_SERIALIZER(AAuraEffectActor)
+
+
+#define FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAuraEffectActor(AAuraEffectActor&&); \
+	NO_API AAuraEffectActor(const AAuraEffectActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAuraEffectActor); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAuraEffectActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AAuraEffectActor) \
+	NO_API virtual ~AAuraEffectActor();
+
+
+#define FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_11_PROLOG
+#define FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_SPARSE_DATA \
+	FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_ACCESSORS \
+	FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_INCLASS_NO_PURE_DECLS \
+	FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> AURA_API UClass* StaticClass<class AAuraEffectActor>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_gavyb_OneDrive_Desktop_Tutorials_Unreal_GAS_Course_Aura_Source_Aura_Public_Actors_AuraEffectActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
